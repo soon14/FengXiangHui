2c284736f3c44bd0932284fd0ce27e429245fea4
孙湖滨 <fst_hangeng@hotmail.com>
2018-07-09 11:19:04 +0800

    1、创建项目

---
+++ FengXH/FengXH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FengXH
+//
+//  Created by 孙湖滨 on 2018/7/9.
+//  Copyright © 2018年 HubinSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FengXH/FengXH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FengXH
+//
+//  Created by 孙湖滨 on 2018/7/9.
+//  Copyright © 2018年 HubinSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
